4017cf1e1a256fd6543c16e161cd2a3b468a622a
CynthiaK <khancynthia28@gmail.com>
2019-06-18 23:31:01 -0400

    Create bulk_twitter_userid_to_screenname.r

---
+++ bulk_twitter_userid_to_screenname.r
@@ -0,0 +1,24 @@
+library(rtweet)
+library(tidyverse)
+library(devtools)
+library(twitteR)
+
+
+token <- create_token(
+  app = "hurricane_migration",
+  consumer_key = "###########################",
+  consumer_secret = "####################################",
+  access_token = "####################################",
+  access_secret = "################################")
+
+irmares <- readRDS("/home/cynthiak/Irma/residents.Rds")
+
+colnames(irmares)
+
+users <- lookup_users(irmares, token = token)
+
+print(users$screen_name)
+
+irma_names <- users$screen_name
+
+write.csv(irma_names, file = "/home/cynthiak/IrmaNames.csv")
